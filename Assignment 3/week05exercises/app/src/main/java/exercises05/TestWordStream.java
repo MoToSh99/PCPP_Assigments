c464b4f604e5c9bd029208c2d5b98571bb369e9d
Toheed Sharif <info@toheed.dk>
2021-09-27 13:43:39 +0200

    week 5man done

--- Assignment 3/week05exercises/app/src/main/java/exercises05/TestWordStream.java
+++ Assignment 3/week05exercises/app/src/main/java/exercises05/TestWordStream.java
@@ -5,11 +5,7 @@ package exercises05;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.IntSummaryStatistics;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -17,27 +13,64 @@ import java.util.stream.Stream;
 public class TestWordStream {
   public static void main(String[] args) {
     String filename = "src/main/resources/english-words.txt";
+    //Exercise 1
     System.out.println(readWords(filename).count());
+
+    //Exercise 2
+    readWords(filename).limit(100).forEach(System.out::println);
+
+    //Exercise 3
+    readWords(filename).filter(x -> x.length() > 21).forEach(System.out::println);
+
+    //Exercise 4
+    readWords(filename).filter(x -> x.length() > 21).limit(1).forEach(System.out::println);
+
+    //Exercise 5
+    long startTime1 = System.nanoTime();
+    readWords(filename).filter(x -> isPalindrome(x)).forEach(System.out::println);
+    long endTime1 = System.nanoTime();
+    long duration1 = (endTime1 - startTime1)/1000000;
+    
+    //Exercise 6
+    long startTime = System.nanoTime();
+    readWords(filename).parallel().filter(x -> isPalindrome(x)).forEach(System.out::println);
+    long endTime = System.nanoTime();
+    long duration = (endTime - startTime)/1000000;
+    System.out.println(duration1);
+    System.out.println(duration);
+    
+    //Exercise 7
+    System.out.println(readWords(filename).mapToInt(x -> x.length()).min());
+    System.out.println(readWords(filename).mapToInt(x -> x.length()).max());
+    System.out.println(readWords(filename).mapToInt(x -> x.length()).average());
+
+    // Exercise 8
+    System.out.println(readWords(filename).collect(Collectors.groupingBy(x -> x.length())));
+    
+    //Exercise 9
+    readWords(filename).map(x -> letters(x)).limit(100).forEach(System.out::println);
+
+    //Exercise 10
+    System.out.println(readWords(filename).map(x -> letters(x).get('e')).filter(x -> x != null).reduce(0, (a, b) -> a + b));
   }
 
   public static Stream<String> readWords(String filename) {
     try {
       BufferedReader reader = new BufferedReader(new FileReader(filename));
-      // TO DO: Implement properly
-      return Stream.<String>empty();
+      return reader.lines();
     } catch (IOException exn) { 
       return Stream.<String>empty();
     }
   }
 
   public static boolean isPalindrome(String s) {
-    // TO DO: Implement properly
-    return false; 
+    StringBuffer stringBuffer = new StringBuffer(s);
+    return stringBuffer.reverse().toString().equals(s);
   }
 
   public static Map<Character,Integer> letters(String s) {
     Map<Character,Integer> res = new TreeMap<>();
-    // TO DO: Implement properly
+    for (char c : s.toLowerCase().toCharArray()) res.put(c, res.getOrDefault(c, 0) + 1);
     return res;
   }
 }
