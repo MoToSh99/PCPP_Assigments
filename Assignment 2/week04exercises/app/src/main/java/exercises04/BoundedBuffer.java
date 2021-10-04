b1869598f74677067ce53ad1e06a6c7d9a1c69a7
Toheed Sharif <info@toheed.dk>
2021-10-04 11:57:39 +0200

    week 06 init

--- Assignment 2/week04exercises/app/src/main/java/exercises04/BoundedBuffer.java
+++ Assignment 2/week04exercises/app/src/main/java/exercises04/BoundedBuffer.java
@@ -8,16 +8,20 @@ public class BoundedBuffer implements BoundedBufferInteface {
     private Queue<Object> list = new LinkedList<Object>();
     private Semaphore producers;
     private Semaphore consumers;
+    private Semaphore lock;
 
     public BoundedBuffer(int size){
         producers = new Semaphore(size);   
-        consumers = new Semaphore(0);   
+        consumers = new Semaphore(0); 
+        lock = new Semaphore(1);  
     }
 
     @Override
     public Object take() throws Exception {
         consumers.acquire();
+        lock.acquire();
         Object o = list.poll();
+        lock.release();
         producers.release();
         return o;
     }
@@ -25,7 +29,9 @@ public class BoundedBuffer implements BoundedBufferInteface {
     @Override
     public void insert(Object elem) throws Exception {
         producers.acquire();
+        lock.acquire();
         list.offer(elem);
+        lock.release();
         consumers.release();
     }
     
