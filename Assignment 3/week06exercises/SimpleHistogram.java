98efb046e818675b52b190392e84aebe9bdd6f09
conr <conr@itu.dk>
2021-10-06 13:23:43 +0200

    Week 6 done

--- Assignment 3/week06exercises/SimpleHistogram.java
+++ Assignment 3/week06exercises/SimpleHistogram.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,35 +17,30 @@ interface Histogram {
 
 public class SimpleHistogram {
 	public static void main(String[] args) {
-		final Histogram histogram = new Histogram2(30);
+		final Histogram histogram = new Histogram2(5_000_000);
         dump(countParallelN(5_000_000, 30, histogram));
     }
-    
-    public static Histogram countParallelN(int range, int threadCount,
-			final Histogram histogram) {
-		final int perThread = range / threadCount;
 
+	private static Histogram countParallelN(int range, int threadCount, Histogram histogram) {
+		final int perThread = range / threadCount;
 		Thread[] threads = new Thread[threadCount];
-		for (int t = 0; t < threadCount; t++) {
-			final int from = perThread * t, to = (t + 1 == threadCount) ? range
-					: perThread * (t + 1);
-			threads[t] = new Thread(new Runnable() {
-				public void run() {
-					for (int i = from; i < to; i++)
-						histogram.increment(countFactors(i));
-				}
+		for (int t=0; t<threadCount; t++) {
+			final int from = perThread * t, 
+				to = (t+1==threadCount) ? range : perThread * (t+1); 
+			threads[t] = new Thread( () -> {
+				for (int i=from; i<to; i++)
+					histogram.increment(countFactors(i));
 			});
 		}
-		for (int t = 0; t < threadCount; t++)
-			threads[t].start();
+		for (int t=0; t<threadCount; t++) 
+		  threads[t].start();
 		try {
-			for (int t = 0; t < threadCount; t++)
-				threads[t].join();
-		} catch (InterruptedException exn) {
-		}
+		  for (int t=0; t<threadCount; t++) 
+			threads[t].join();
+		} catch (InterruptedException exn) { }
 		return histogram;
-	}
-
+	  }
+	
 	public static void dump(Histogram histogram) {
 		for (int bin = 0; bin < histogram.getSpan(); bin++) {
 			System.out.printf("%4d: %9d%n", bin, histogram.getCount(bin));
