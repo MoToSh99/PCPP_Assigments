dc3664b30a70a09e39c7b0a636d08e516cc463ca
Toheed Sharif <info@toheed.dk>
2021-11-08 12:03:25 +0100

    week 10

---
+++ Assignment 5/week10exercises/app/src/main/java/mobilepayment/Bank.java
@@ -0,0 +1,31 @@
+package mobilepayment;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.*;
+
+public class Bank extends AbstractBehavior</*To be Implemented*/> {
+
+    /* --- Messages ------------------------------------- */
+    // To be Implemented
+    
+
+    /* --- State ---------------------------------------- */
+    // To be Implemented
+    
+
+    /* --- Constructor ---------------------------------- */
+    // To be Implemented
+    
+
+    /* --- Actor initial behavior ----------------------- */
+    // To be Implemented
+    
+
+    /* --- Message handling ----------------------------- */
+    // To be Implemented
+    
+
+    /* --- Handlers ------------------------------------- */
+    // To be Implemented
+}
---
+++ Assignment 5/week10exercises/app/src/main/java/mobilepayment/MobileApp.java
@@ -0,0 +1,35 @@
+package mobilepayment;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.*;
+
+// Hint: You may generate random numbers using Random::ints
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class MobileApp extends AbstractBehavior</*To be implemented*/> {
+
+    /* --- Messages ------------------------------------- */
+    // To be Implemented
+    
+
+    /* --- State ---------------------------------------- */
+    // To be Implemented
+    
+
+    /* --- Constructor ---------------------------------- */
+    // To be Implemented
+    
+
+    /* --- Actor initial behavior ----------------------- */
+    // To be Implemented
+    
+
+    /* --- Message handling ----------------------------- */
+    // To be Implemented
+    
+
+    /* --- Handlers ------------------------------------- */
+    // To be Implemented
+}
